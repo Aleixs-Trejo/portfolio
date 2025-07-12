7f4323ba03d4f796db539b202c37920008c0fc1f
Aleixs-Trejo <66097331+Aleixs-Trejo@users.noreply.github.com>
2025-07-12 15:47:35 -0500

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,17 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import preact from '@astrojs/preact';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aleixs-trejo.github.io',
   base: '/portfolio',
+
   vite: {
     plugins: [tailwindcss()]
   },
-  integrations: [preact()]
+
+  integrations: [preact()],
+  adapter: vercel()
 });
